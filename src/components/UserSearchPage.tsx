0e975d097bb3433637ceb00e37d9c20bd5ce9a8c
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-10-12 14:53:13 +0530

    placeholder component for user search page and style cleanup

---
+++ src/components/UserSearchPage.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import "../styles/user-search.css";
+
+export default class UserSearchPage extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className="user-search">
+        <h2>User Search</h2>;
+      </div>
+    );
+  }
+}
