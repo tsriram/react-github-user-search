d5feafdfc2d01b39557ce3e332086256bbb751f2
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-10-13 15:51:06 +0530

    fire search query manually on submit

--- src/components/UserSearchPage.tsx
+++ src/components/UserSearchPage.tsx
@@ -1,25 +1,55 @@
 import { SearchUsersQuery } from "src/queries";
+import { ApolloClient } from "apollo-boost";
 import SearchResult from "./SearchResult";
-import { Query } from "react-apollo";
+import { withApollo } from "react-apollo";
 import SearchBar from "./SearchBar";
 import * as React from "react";
 
 import "../styles/user-search.css";
 
-interface SearchPageState {
-  readonly query: string;
-  readonly shouldFetchData: boolean;
+interface SearchNode {
+  readonly node: {
+    readonly name: string;
+    readonly email: string;
+    readonly bio: string;
+    readonly login: string;
+    readonly avatarUrl: string;
+    readonly location: string;
+    readonly url: string;
+  };
 }
 
-export default class UserSearchPage extends React.Component<
-  {},
-  SearchPageState
-> {
+interface SearchResults {
+  readonly search: {
+    readonly userCount: number | undefined;
+    readonly edges: ReadonlyArray<SearchNode>;
+  };
+}
+
+interface SearchPageState {
+  readonly query: string;
+  readonly loading: boolean;
+  readonly error: boolean;
+  readonly data: SearchResults;
+}
+
+interface SearchPageProps {
+  readonly client: ApolloClient<SearchResults>;
+}
+
+class UserSearchPage extends React.Component<SearchPageProps, SearchPageState> {
   constructor(props: any) {
     super(props);
     this.state = {
       query: "",
-      shouldFetchData: false
+      loading: false,
+      error: false,
+      data: {
+        search: {
+          userCount: undefined,
+          edges: []
+        }
+      }
     };
   }
 
@@ -31,13 +61,58 @@ export default class UserSearchPage extends React.Component<
 
   handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    this.setState({
-      shouldFetchData: true
-    });
+    this.executeSearch();
   };
 
-  public render() {
+  executeSearch = async () => {
+    this.setState({
+      loading: true
+    });
     const query = this.state.query;
+    try {
+      const { data } = await this.props.client.query<SearchResults>({
+        query: SearchUsersQuery,
+        variables: { query }
+      });
+      this.setState({
+        loading: false,
+        data
+      });
+    } catch (error) {
+      console.error("error: ", error);
+      this.setState({
+        loading: false,
+        error: true
+      });
+    }
+  };
+
+  private renderLoading() {
+    return this.state.loading ? <h3>Loading...</h3> : null;
+  }
+
+  private renderError() {
+    return this.state.error ? <h3>Error loading search results</h3> : null;
+  }
+
+  private renderSearchResults() {
+    const { data } = this.state;
+    if (data.search.userCount !== undefined && data.search.userCount > 0) {
+      return (
+        <div>
+          {data.search.edges.map((edge: any) => {
+            return <SearchResult userNode={edge.node} key={edge.node.login} />;
+          })}
+        </div>
+      );
+    } else if (data.search.userCount === 0) {
+      return <h3>No results found!</h3>;
+    } else {
+      return null;
+    }
+  }
+
+  public render() {
     return (
       <div className="user-search page-container">
         <h2 className="title">GitHub User Search</h2>
@@ -46,31 +121,13 @@ export default class UserSearchPage extends React.Component<
           onChange={this.handleQueryChange}
           onSubmit={this.handleSubmit}
         />
-        {this.state.shouldFetchData && (
-          <Query query={SearchUsersQuery} variables={{ query }}>
-            {({ loading, error, data }) => {
-              if (loading) {
-                return <h3>Loading...</h3>;
-              } else if (error) {
-                return <h3>Error loading search results</h3>;
-              } else {
-                return (
-                  <div>
-                    {data.search.edges.map((edge: any) => {
-                      return (
-                        <SearchResult
-                          userNode={edge.node}
-                          key={edge.node.login}
-                        />
-                      );
-                    })}
-                  </div>
-                );
-              }
-            }}
-          </Query>
-        )}
+
+        {this.renderLoading() ||
+          this.renderError() ||
+          this.renderSearchResults()}
       </div>
     );
   }
 }
+
+export default withApollo(UserSearchPage);
