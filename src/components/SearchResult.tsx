31394dc88241657cedbc3ea6e90a2f5235bfff1b
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-10-14 12:04:11 +0530

    add react-icons

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,3 +1,5 @@
+import { IconContext } from "react-icons";
+import { GoMail, GoLocation } from "react-icons/go";
 import * as React from "react";
 
 interface SearchResultProps {
@@ -16,28 +18,40 @@ const SearchResult: React.SFC<SearchResultProps> = props => {
   const { name, email, bio, login, avatarUrl, location, url } = props.userNode;
 
   return (
-    <div className="search-result">
-      <img className="avatar" alt={name} src={`${avatarUrl}&s=96`} />
-      <div className="user-info">
-        <div className="row">
-          <a className="user-login" href={url} target="_blank">
-            {login}
-          </a>
-          <span className="user-name">{name}</span>
-        </div>
-
-        {bio && (
+    <IconContext.Provider value={{ className: "react-icons-go" }}>
+      <div className="search-result">
+        <img className="avatar" alt={name} src={`${avatarUrl}&s=96`} />
+        <div className="user-info">
           <div className="row">
-            <div className="user-bio">{bio}</div>
+            <a className="user-login" href={url} target="_blank">
+              {login}
+            </a>
+            <span className="user-name">{name}</span>
           </div>
-        )}
 
-        <div className="row">
-          {location && <div className="user-location">{location}</div>}
-          {email && <div className="user-email">{email}</div>}
+          {bio && (
+            <div className="row">
+              <div className="user-bio">{bio}</div>
+            </div>
+          )}
+
+          <div className="row">
+            {location && (
+              <div className="user-location">
+                <GoLocation />
+                {location}
+              </div>
+            )}
+            {email && (
+              <div className="user-email">
+                <GoMail />
+                {email}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </IconContext.Provider>
   );
 };
 
