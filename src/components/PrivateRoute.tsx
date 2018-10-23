ff197603a10cdcaba7b7acfe015ba3274b72c119
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-10-23 22:00:56 +0530

    custom Route component for authenticated routes

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,38 @@
+import {
+  Route,
+  Redirect,
+  RouteProps,
+  RouteComponentProps
+} from "react-router-dom";
+import { auth } from "../services/Auth";
+import * as React from "react";
+
+interface PrivateRouteProps extends RouteProps {
+  readonly component:
+    | React.ComponentType<RouteComponentProps<any>>
+    | React.ComponentType<any>;
+}
+
+const PrivateRoute = (props: PrivateRouteProps) => {
+  const { component, ...rest } = props;
+  const Component = component;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        auth.isAuthenticated() && Component ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
