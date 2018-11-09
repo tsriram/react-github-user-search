3e19344f7197fddb7e2cab1d56d15a05bd433883
Sriram Thiagarajan <sri.sjc@gmail.com>
2018-11-09 14:54:00 +0530

    initialize apollo client only when github token is available

--- src/components/UserSearchPage.tsx
+++ src/components/UserSearchPage.tsx
@@ -4,20 +4,42 @@ import ApolloClient from "apollo-boost";
 import UserSearch from "./UserSearch";
 import * as React from "react";
 
-const gitHubToken = auth.getAccessToken();
-const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  headers: {
-    authorization: `Bearer ${gitHubToken}`
-  }
-});
+interface UserSearchPageState {
+  readonly client: ApolloClient<any> | null;
+}
 
-export default class UserSearchPage extends React.Component<{}, {}> {
+export default class UserSearchPage extends React.Component<
+  {},
+  UserSearchPageState
+> {
+  state = {
+    client: null
+  };
+
+  componentDidMount() {
+    const gitHubToken = auth.getAccessToken();
+    if (gitHubToken) {
+      this.setState({
+        client: new ApolloClient({
+          uri: "https://api.github.com/graphql",
+          headers: {
+            authorization: `Bearer ${gitHubToken}`
+          }
+        })
+      });
+    }
+  }
+
   public render() {
-    return (
-      <ApolloProvider client={client}>
-        <UserSearch />
-      </ApolloProvider>
-    );
+    const { client } = this.state;
+    if (client !== null) {
+      return (
+        <ApolloProvider client={client}>
+          <UserSearch />
+        </ApolloProvider>
+      );
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
