17c147f455a70b203e7803b549fe9d05f6c7bbb6
Sriram Thiagarajan <sri.sjc@gmail.com>
2019-02-01 16:15:01 +0530

    make UserSearchPage simple, because why not?

--- src/components/UserSearchPage.tsx
+++ src/components/UserSearchPage.tsx
@@ -4,42 +4,24 @@ import ApolloClient from "apollo-boost";
 import UserSearch from "./UserSearch";
 import * as React from "react";
 
-interface UserSearchPageState {
-  readonly client: ApolloClient<any> | null;
-}
-
-export default class UserSearchPage extends React.Component<
-  {},
-  UserSearchPageState
-> {
-  state = {
-    client: null
-  };
-
-  componentDidMount() {
-    const gitHubToken = auth.getAccessToken();
-    if (gitHubToken) {
-      this.setState({
-        client: new ApolloClient({
-          uri: "https://api.github.com/graphql",
-          headers: {
-            authorization: `Bearer ${gitHubToken}`
-          }
-        })
-      });
-    }
+const gitHubToken = auth.getAccessToken();
+const apolloClient = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: `Bearer ${gitHubToken}`
   }
+});
 
-  public render() {
-    const { client } = this.state;
-    if (client !== null) {
-      return (
-        <ApolloProvider client={client}>
-          <UserSearch />
-        </ApolloProvider>
-      );
-    } else {
-      return <div>Loading...</div>;
-    }
+const UserSearchPage = () => {
+  if (apolloClient !== null) {
+    return (
+      <ApolloProvider client={apolloClient}>
+        <UserSearch />
+      </ApolloProvider>
+    );
+  } else {
+    return <div>Loading...</div>;
   }
-}
+};
+
+export default UserSearchPage;
