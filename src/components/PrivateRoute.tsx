824395b90a3e22632c55fd8f39bfc6bac0359e8a
Sriram Thiagarajan <sri.sjc@gmail.com>
2019-02-01 12:50:18 +0530

    useContext hook in PrivateRoute

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,21 +1,15 @@
 import { RouteProps } from "react-router-dom";
-import { AuthContext } from "src/AuthContext";
+import { AuthContext } from "../AuthContext";
 import * as React from "react";
 import Login from "./Login";
 
-class PrivateRoute extends React.Component<RouteProps, {}> {
-  render() {
-    const { component, ...rest } = this.props;
-    // TODO: fix any
-    const Component: any = component;
-    return (
-      <AuthContext.Consumer>
-        {user => {
-          return user ? <Component {...rest} /> : <Login />;
-        }}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const PrivateRoute = (props: RouteProps) => {
+  const user = React.useContext(AuthContext);
+  const { component, ...rest } = props;
+
+  // TODO: fix any
+  const Component: any = component;
+  return user ? <Component {...rest} /> : <Login />;
+};
 
 export default PrivateRoute;
